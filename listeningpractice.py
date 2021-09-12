7ddd7c7724f243557a8b9677a7d7af349873a9fc
aire <72807145+aire-inresonance@users.noreply.github.com>
2021-09-12 16:21:16 +0700

    Update listeningpractice.py
    Upgraded?

--- listeningpractice.py
+++ listeningpractice.py
@@ -1,14 +1,14 @@
 from numpy import random
 from gtts import gTTS
 import os
-
+import collections
 # Default numbers for practice = 5.
-numbers = random.randint(100, size=(5)) 
+number_of_numbers = 5
 
-string = "   ".join([str(i) for i in numbers])
+answer = random.randint(100, size=(number_of_numbers)) 
 
-# Space to prevent the audio file from an abrupt end.
-string = string + " " 
+string = "  ".join([str(i) for i in answer])
+
 
 # Default language = French.
 language = 'fr' 
@@ -19,6 +19,22 @@ output = gTTS(text = string, lang= language, slow=False)
 output.save("lesnombresenfrancaisfast.mp3")
 os.system("start lesnombresenfrancaisfast.mp3")
 
-print(numbers)
+
+
+guess = []
+# Guessing input 
+for ii in range (0, number_of_numbers):
+    guess_num = int(input("Insert each number, end with SPACE: "))
+    guess.append(guess_num)
+
+# Output
+if collections.Counter(guess) == collections.Counter(answer):
+    print("Correct!")
+else:
+    print("False!")
+
+print('Your guess:', guess)
+print('The answer', list(answer))
+
 
 
